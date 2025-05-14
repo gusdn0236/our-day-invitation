58968cd0783492f92708bc8c7e2f9f49f8bfc858
gusdn0236 <hdno0236@naver.com>
2025-05-14 22:38:01 +0900

    배포전 에러잡는중

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// vite.config.js
 export default defineConfig({
-  base: '/our-day-invitation/', // 이게 매우 중요!
+  base: '/our-day-invitation/', // 반드시 슬래시 포함해서 정확히 써야 함
   plugins: [react()],
 });
