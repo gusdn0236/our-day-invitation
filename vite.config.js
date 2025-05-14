6550ee0609ae3d1fc12d7a40d842374d46b60829
gusdn0236 <hdno0236@naver.com>
2025-05-14 23:06:11 +0900

    배포 완료

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// vite.config.js
 export default defineConfig({
-  base: '/our-day-invitation/', // 반드시 슬래시 포함해서 정확히 써야 함
   plugins: [react()],
+  base: '/our-day-invitation/', // ← 레포 이름에 맞게 base 경로 설정!
 });
